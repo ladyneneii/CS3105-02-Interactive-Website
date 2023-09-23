10969f09f5c249cc336a8880d17a3a97cfe9c3a4
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-23 17:47:13 +0800

    Added card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import "../styles/components/card.css";
+import jiafei from "../assets/img/jiafei-498x486.webp";
+import { Skeleton } from "@mui/material";
+
+interface Props {
+  load: number;
+}
+
+const Card = ({ load }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [imgWidth, setImgWidth] = useState(286);
+  const [imgHeight, setImgHeight] = useState(279);
+
+  const [titleWidth, setTitleWidth] = useState(254);
+  const [titleHeight, setTitleHeight] = useState(24);
+
+  const [textWidth, setTextWidth] = useState(254);
+  const [textHeight, setTextHeight] = useState(72);
+
+  useEffect(() => {
+    const img = document.querySelector<HTMLImageElement>(".card-img-top");
+    const title = document.querySelector<HTMLHeadingElement>(".card-title");
+    const text = document.querySelector<HTMLHeadingElement>(".card-text");
+
+    if (img) {
+      setImgWidth(img.getBoundingClientRect().width);
+      setImgHeight(img.getBoundingClientRect().height);
+    }
+
+    if (title) {
+      setTitleWidth(title.getBoundingClientRect().width);
+      setTitleHeight(title.getBoundingClientRect().height);
+    }
+
+    if (text) {
+      setTextWidth(text.getBoundingClientRect().width);
+      setTextHeight(text.getBoundingClientRect().height);
+    }
+
+    setTimeout(() => {
+      setIsLoading(true);
+    }, load);
+
+    console.log(textWidth, textHeight);
+  }, [load]);
+
+  return (
+    <>
+      <div className="card card__width">
+        {isLoading ? (
+          <img src={jiafei} className="card-img-top" alt="our kween jiafei" />
+        ) : (
+          <Skeleton
+            variant="rounded"
+            animation="wave"
+            className="skeleton card__width"
+            width={imgWidth}
+            height={imgHeight}
+          />
+        )}
+        <div className="card-body">
+          {isLoading ? (
+            <h5 className="card-title">Card title</h5>
+          ) : (
+            <Skeleton
+              variant="rounded"
+              animation="wave"
+              width={titleWidth}
+              height={titleHeight}
+              className="card-title skeleton"
+            />
+          )}
+          {isLoading ? (
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          ) : (
+            <Skeleton
+              variant="rounded"
+              animation="wave"
+              width={textWidth}
+              height={textHeight}
+              className="skeleton"
+            />
+          )}
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,28 @@
-const MainPage = () => {
-  return (
-    <div>MainPage</div>
-  )
-}
+import { useState, useRef, useEffect } from "react";
+import Card from "../components/Card";
 
-export default MainPage
+const MainPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(true);
+  //   }, 2000);
+  // }, []);
+
+  return (
+    <>
+      <div className="container text-center">
+        <div className="row">
+          {[...Array(12)].map((_, index) => (
+            <div className="col" key={index}>
+              <Card load={2000} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
