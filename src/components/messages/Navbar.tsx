bbd0d8ab703b758c1f70f978533db9b76cfd1342
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-09-29 22:35:08 +0800

    Finished design for Navbar and Search components

--- src/components/messages/Navbar.tsx
+++ src/components/messages/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import trisha from "../../assets/img/trisha-3328x1872.jpg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">Message</span>
+      <div className="user">
+        <img className="img" src={trisha} alt="" />
+        <span>John</span>
+        <button className='button'>logout</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/messages/Search.tsx
+++ src/components/messages/Search.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import jiafei from "../../assets/img/jiafei-498x486.webp"
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input className="input" type="text" placeholder="Find a user..." />
+      </div>
+      <div className="userChat">
+        <img className="img" src={jiafei} alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
