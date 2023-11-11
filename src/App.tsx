4e3caed066fb180508557c5e1fd9187349d0ca00
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-11-11 18:01:04 +0800

    updated submodule

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import SignIn from "./pages/SignIn";
 import MainPage from "./pages/MainPage";
 import CommentsPage from "./pages/CommentsPage";
 import MessagesPage from "./pages/MessagesPage";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 const App = () => {
   return (
